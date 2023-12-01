d3a923203c5accdb9502c34ec950aa8b676c0015
dev-givers-health <dev@givers.health>
2023-12-01 08:26:36 +0530

    changes in signup mixin class

--- lib/app/modules/user_account_modules/cg_profile_modules/profile_update/services/profile_update_services.dart
+++ lib/app/modules/user_account_modules/cg_profile_modules/profile_update/services/profile_update_services.dart
@@ -15,8 +15,6 @@ import '../../profile/services/mixin_get_cg_profile.dart';
 
 class ProfileUpdateServices extends GetxController with MixinGetCgProfile{
   bool hasAnythingChanged = false;
-  // final MenuServices userBioServicesController = Get.find();
-  // final ProfileImageUploadFirebase _profileImageUploadFirebase = Get.put(ProfileImageUploadFirebase());
   late String cgZipcode;
   late String dob;
   late String cgFirstName;
