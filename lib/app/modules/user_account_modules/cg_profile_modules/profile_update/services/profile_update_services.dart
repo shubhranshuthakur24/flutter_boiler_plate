654986362cd21bfbf1bfbdf7ca5772394c4f643e
dev-givers-health <dev@givers.health>
2023-12-01 08:32:22 +0530

    changes in signup mixin class

--- lib/app/modules/user_account_modules/cg_profile_modules/profile_update/services/profile_update_services.dart
+++ lib/app/modules/user_account_modules/cg_profile_modules/profile_update/services/profile_update_services.dart
@@ -35,6 +35,7 @@ class ProfileUpdateServices extends GetxController with MixinGetCgProfile{
   onInit() {
     super.onInit();
     apiGetCgProfile();
+    apiUpdateUserDetailsWithImage(zipcode: '', dob: '', contactNumber: '');
 
 
     // try {
